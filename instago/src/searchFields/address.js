a4a7c266f970894de3763d0e131c49829c44a405
Jackie Chen <jackie.chen73@gmail.com>
2019-11-23 03:26:04 -0500

    3:26am

---
+++ instago/src/searchFields/address.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class AddressField extends Component {
+	render() {
+		return(
+			<div>
+				{this.props.message}
+				Welcome!
+			</div>
+		);
+	}
+}
+
+export default AddressField;
